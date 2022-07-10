e9a03c0bf58c91891f9b82354c4ffec9508ab768
ihorandrianov <andrianoia@gmail.com>
2022-07-10 19:44:29 +0300

    lol

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
+import { Hears, Help, On, Start } from 'nestjs-telegraf';
+import { Context, Telegraf } from 'telegraf';
+
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  @Start()
+  async startCommand(ctx: Context) {
+    await ctx.reply('welcome')
   }
+  @Help()
+  async helpCommand(ctx: Context) {
+    await ctx.reply('Send me a sticker')
+  }
+
+  @On('sticker')
+  async onSticker(ctx: Context) {
+    await ctx.reply('cool')
+  }
+
+  @Hears('hi')
+  async hearsHi(ctx: Context) {
+    await ctx.reply('Hey there');
+  }
+
+
+  
+  
+  
+  
 }
