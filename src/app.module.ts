7c04f014568c60071c90e35da03485f3fcbcebd0
ihorandrianov <andrianoia@gmail.com>
2022-07-11 20:34:51 +0300

    hk

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [TelegrafModule.forRootAsync({
     imports: [ConfigModule],
-    useFactory: (configService: ConfigService) => ({
-      token: configService.get<string>('BOT_TOKEN'),
-    
+    useFactory: async (configService: ConfigService) => ({
+      token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
     }),
     inject: [ConfigService],
-  });]
-  ,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
